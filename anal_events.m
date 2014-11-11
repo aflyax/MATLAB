a54e4693e7011557cf1d14a764eff7cdc1e73e54
Alexander Flyax <aflyax@gmail.com>
2014-11-10 20:39:40 -0600

    uploading matlab files

--- anal_events.m
+++ anal_events.m
@@ -1,3 +1,5 @@
+
+
 %% get the data, init constants
 
 clear all;
@@ -22,6 +24,7 @@ smooth_deg = 0.005;
 % find peak at min (stimulus start : peak_end)
 
 figure; hold on;
+xlim([data_start, data_finish])
 grey=[.8,.8,.8];
 
 for i = 1 : size (fig_y)
@@ -33,37 +36,26 @@ for i = 1 : size (fig_y)
     
     %     data_peaks_x(i) = find (data_sweeps{i} == data_peaks_y(i));
     data_peaks_x(i) = data_peaks_x(i) + stim_start;
+    
     plot (data_sweeps{i}, 'Color', grey);
     plot (data_peaks_x(i), data_peaks_y(i), 'ob');
     
     
     %% finding the onset
     
-    data_pos = pos * data_sweeps{i};
-    
-    d_smooth = smooth (data_pos, smooth_deg, 'loess');
-    diff_s = diff (d_smooth);
-    
-    min_diff = std (diff_s(data_start:stim_start));
-    
-     
-    diff_pts = find (diff_s (stim_start:peak_end) > min_diff);
-    
-    if diff_pts
-        event_onset_x (i) = diff_pts(1,1) + stim_start;
-        event_onset_y (i) = data_sweeps{i}(event_onset_x(i));
-
-        plot (event_onset_x (i), event_onset_y (i), 'ko');
-    else
-        event_onset_x (i) = nan;
-        event_onset_y (i) = nan;
-    end
+    [event_onset_x(i), event_onset_y(i)] = find_onset (data_sweeps{i}, pos);
+    plot (event_onset_x(i), event_onset_y(i), 'ob');
 end
+
+%% average and plot data
+
+% figure(); hold on;
 
 avg_peak_x = mean (data_peaks_x);
 avg_peak_y = mean (data_peaks_y);
 
 plot (avg_peak_x, avg_peak_y, 'bs');
+
 
 avg_onset_x = nanmean (event_onset_x);
 avg_onset_y = nanmean (event_onset_y);
@@ -71,39 +63,25 @@ avg_onset_y = nanmean (event_onset_y);
 plot (avg_onset_x, avg_onset_y, 'ks');
 
 
-%% plotting the data and the average trace
-
 dim = ndims(data_sweeps{2});         %# Get the number of dimensions for your arrays
 M = cat(dim+1,data_sweeps{:});            %# Convert to a (dim+1)-dimensional matrix
 y_mean = mean(M,dim+1);      %# Get the mean across arrays
 
 plot (y_mean, 'r');
 
-[y_mean_peak_y y_mean_peak_x] = min (y_mean(stim_start:peak_end));
+[y_mean_peak_y, y_mean_peak_x] = min (y_mean(stim_start:peak_end));
 y_mean_peak_x = y_mean_peak_x + stim_start;
-
-% y_mean_peak_x = find (y_mean==y_mean_peak_y);
-
-
 
 plot (y_mean_peak_x, y_mean_peak_y, 'or');
 
-%     data_pos = pos * y_mean(stim_start:data_finish);
-    
-    data_pos = pos * y_mean();
-    
-    d_smooth = smooth (data_pos, smooth_deg, 'loess');
-    diff_s = diff (d_smooth);
-    
-    min_diff = std (diff_s(data_start:stim_start));
-    
-    diff_pts = find (diff_s > min_diff);
-    
-    event_onset_x = diff_pts(1,1) + stim_start;
-    event_onset_y = y_mean(event_onset_x);
-    
-    plot (event_onset_x, event_onset_y, 'rs');
 
-%% set axis limits
+[y_mean_onset_x y_mean_onset_y] = find_onset(y_mean, pos);
+plot (y_mean_onset_x, y_mean_onset_y, 'or');
 
-xlim([data_start, data_finish])
+
+%% results
+
+avg_onset_x
+avg_onset_y
+avg_peak_x
+avg_peak_y
